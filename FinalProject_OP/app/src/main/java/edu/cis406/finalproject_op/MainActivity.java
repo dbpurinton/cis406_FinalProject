d953a67b93cd51db9296ed961f92c2c36e01b917
TylerOrcutt <t.orcutt@eagle.clarion.edu>
2015-11-25 15:43:47 -0500

    small changes

--- FinalProject_OP/app/src/main/java/edu/cis406/finalproject_op/MainActivity.java
+++ FinalProject_OP/app/src/main/java/edu/cis406/finalproject_op/MainActivity.java
@@ -1,22 +1,32 @@
 package edu.cis406.finalproject_op;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//this.requestWindowFeature(Window.FEATURE_NO_TITLE);
- //  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+       // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     //    setContentView(R.layout.activity_main);
 
-        setContentView(new GameView(this));
+
+
+if(hasGLES20()) {
+    setContentView(new GameView(this));
+}else{
+    Toast.makeText(this,"This device does not support OpenGL ES 2.0.",Toast.LENGTH_LONG).show();
+}
     }
 
     @Override
@@ -39,5 +49,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean hasGLES20() {
+        ActivityManager am = (ActivityManager)
+                getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+        return info.reqGlEsVersion >= 0x20000;
     }
 }
